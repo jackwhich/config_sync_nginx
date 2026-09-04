bea51950db8a8a3290e95518961729e37b541dfd
kino <zhongzhenx.liu@intel.com>
2026-09-04 18:38:55 +0800

    Add independent publication target domain model

---
+++ internal/domain/target/target.go
@@ -0,0 +1,34 @@
+package target
+
+import (
+	"nginx_updata_config/internal/domain/release"
+	"os"
+	"strconv"
+)
+
+type HealthCheck struct {
+	URL           string `yaml:"url" json:"url"`
+	Host          string `yaml:"host" json:"host,omitempty"`
+	TLSServerName string `yaml:"tls_server_name" json:"tls_server_name,omitempty"`
+	Status        int    `yaml:"status" json:"status"`
+	Contains      string `yaml:"contains" json:"contains,omitempty"`
+}
+type Target struct {
+	Type                release.ReleaseType `yaml:"type" json:"type"`
+	ServerName          string              `yaml:"server_name" json:"server_name"`
+	PathDest            string              `yaml:"path_dest" json:"path_dest"`
+	Project             string              `yaml:"project" json:"project,omitempty"`
+	RequiredFiles       []string            `yaml:"required_files" json:"-"`
+	HealthChecks        []HealthCheck       `yaml:"health_checks" json:"-"`
+	InitialHealthChecks []HealthCheck       `yaml:"initial_health_checks" json:"-"`
+	PublicBaseURL       string              `yaml:"public_base_url" json:"-"`
+	PublicHost          string              `yaml:"public_host" json:"-"`
+	FileMode            string              `yaml:"file_mode" json:"-"`
+	ID                  string              `yaml:"-" json:"target_id"`
+	Dir                 string              `yaml:"-" json:"deployment_dir"`
+}
+
+func (t Target) Mode() os.FileMode {
+	v, _ := strconv.ParseUint(t.FileMode, 8, 32)
+	return os.FileMode(v)
+}
